3d80bba987ab9f7501242beb4739208e11e68267
zaxar396 <zaxar396@yandex.ru>
2016-06-04 17:29:27 +0400

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -28,19 +28,62 @@ int main(int argc, char* argv[])
 {
 	char buff1[100]; 
 	char buff2[100];
+	int p1 = 0;
+	int p2 = 0;
 	ifstream fin1("file1.txt");
 	ifstream fin2("file2.txt");
 	for (int i = 0; i < lines(); i++)
 	{
 		fin1 >> buff1;
-		fin2 >> buff2;
-		if (strcmp(buff1,buff2) != 0 )
+		for (int j = 0; j < lines(); j++)
 		{
-			cout << "file1.txt" << " " << i + 1  << " " << buff1 <<"\n";
-			cout << "file2.txt" <<" "<< i + 1  << " " << buff2 << "\n";
+			fin2 >> buff2;
+			if (strcmp(buff1, buff2) == 0)
+			{
+				break;
+			}
+			else
+			{
+				p1++;
+			}
+		}
+
+		if (p1 == lines())
+		{
+			cout << "file1.txt " << i + 1 << " " << buff1 << endl;
 		}
 
 	}
+
+	fin1.close();
+	fin1.close();
+
+	ifstream fin3("file1.txt");
+	ifstream fin4("file2.txt");
+
+	for (int i = 0; i < lines(); i++)
+	{
+		fin4 >> buff2;
+		for (int j = 0; j < lines(); j++)
+		{
+			fin3 >> buff1;
+			if (strcmp(buff2, buff1) == 0)
+			{
+				break;
+			}
+			else
+			{
+				p2++;
+			}
+		}
+
+		if (p2 == lines())
+		{
+			cout << "file2.txt " << i + 1 << " " << buff2 << endl;
+		}
+	}
+	fin3.close();
+	fin4.close();
 	system("pause");
 	return 0;
 }
